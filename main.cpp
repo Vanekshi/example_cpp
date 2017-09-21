63b3ae397a605aa484bc1720f20ffda684e3b84d
Egor Ishkenov <egori99@yandex.ru>
2017-09-21 13:41:45 +0300

    Add number of input arguments

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    cout << "Hello, World!\n";
+   int a=10,b=20,c;
+   c=a+b;
+    cout << "Hello, World!\n my IQ is"<<'c';
 
     return 0;
 }
